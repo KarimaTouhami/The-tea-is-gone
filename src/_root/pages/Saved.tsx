f03dfe87f10469e6576ba4f5958f079d5229b6a2
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-10-30 15:48:07 +0100

    fix: wrap "No available posts" message in a list item for proper formatting

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -33,7 +33,9 @@ const Saved = () => {
       ) : (
         <ul className="w-full flex justify-center max-w-5xl gap-9">
           {savePosts.length === 0 ? (
-            <p className="text-light-4">No available posts</p>
+            <li>
+              <p className="text-light-4">No available posts</p>
+            </li>
           ) : (
             <GridPostList posts={savePosts} showStats={false} />
           )}
